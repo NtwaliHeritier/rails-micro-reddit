6a2198598f3ae1140766c1beb88294e6e9d42425
NtwaliHeritier <heritierntwali@gmail.com>
2020-10-15 15:54:25 +0200

    add post control and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+    include PostsHelper
+    def new
+        @post=Post.new
+    end
+
+    def create
+        @post=Post.new(post_params)
+        @post.save
+        redirect_to post_path(@post)
+    end
+
+    def show
+        @post=Post.find(params[:id])
+    end
+
+    def index
+        @posts=Post.all
+    end
 end
