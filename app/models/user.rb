77eff790bfef5aefb050cb437c7204511b2f7920
GraceOyiza <graceoyiza13@gmail.com>
2020-10-16 10:17:57 +0100

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    has_many :posts
-    validates :name, presence: true
-    validates :username, presence: true
-    validates :password, presence: true
-    validates :password, presence: true, length: {minimum: 7}
-    validates :country, presence: true
+  has_many :posts
+  validates :name, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 7 }
+  validates :country, presence: true
 end
